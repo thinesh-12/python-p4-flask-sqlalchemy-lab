117d10885a0421cfaf8cc93c488d60f123ed11a4
= <=>
2023-09-03 23:42:47 -0400

    lab

--- server/app.py
+++ server/app.py
@@ -6,29 +6,76 @@ from flask_migrate import Migrate
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return '<h1>Zoo app</h1>'
+    return "<h1>Zoo app</h1>"
 
-@app.route('/animal/<int:id>')
+
+@app.route("/animal/<int:id>")
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if not animal:
+        response_body = "<h1>404 animal not found</h1>"
+        response = make_response(response_body, 404)
+        return response
+    response_body = f"""
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    """
+    response = make_response(response_body, 200)
+    return response
 
-@app.route('/zookeeper/<int:id>')
+
+@app.route("/zookeeper/<int:id>")
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not zookeeper:
+        response_body = "<h1>404 zookeeper not found</h1>"
+        response = make_response(response_body, 404)
+        return response
+    response_body = f"""
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+    """
+    for ani in zookeeper.animals:
+        response_body += f"<ul>Animal: {ani.name}</ul>"
 
-@app.route('/enclosure/<int:id>')
+    response = make_response(response_body, 200)
+
+    return response
+
+
+@app.route("/enclosure/<int:id>")
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    if not enclosure:
+        response_body = "<h1>404 enclosure not found</h1>"
+        response = make_response(response_body, 404)
+        return response
+    response_body = f"""
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {True if enclosure.open_to_visitors else False}</ul>
+    """
+    for ani in enclosure.animals:
+        response_body += f"<ul>Animal: {ani.name}</ul>"
+
+    response = make_response(response_body, 200)
+
+    return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
